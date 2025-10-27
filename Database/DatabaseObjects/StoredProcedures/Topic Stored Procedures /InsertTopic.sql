3156741afbc20ae56fe3d1c65f90d5e5ef8e3629
Lucia <106497242+LuciaRepos@users.noreply.github.com>
2025-10-27 17:56:53 -0300

    Update InsertTopic.sql

--- Database/DatabaseObjects/StoredProcedures/Topic Stored Procedures /InsertTopic.sql
+++ Database/DatabaseObjects/StoredProcedures/Topic Stored Procedures /InsertTopic.sql
@@ -1,1 +1,8 @@
-
+CREATE PROCEDURE InsertTopic
+	@Theme VARCHAR(100),
+	@Content VARCHAR(500)
+AS
+BEGIN
+	INSERT INTO [Topic] (Theme, Content, CreateUser, CreateDate, LastUpdateUser, LastUpdateDate)
+	VALUES (@Theme, @Content, ORIGINAL_LOGIN(), GETDATE(), ORIGINAL_LOGIN(), GETDATE());
+ END
