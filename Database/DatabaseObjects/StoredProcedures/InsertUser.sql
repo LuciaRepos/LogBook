5940d98daec1ab2c2d0e05b5554faa5d971a4e3a
Lucia <106497242+LuciaRepos@users.noreply.github.com>
2025-10-27 16:09:34 -0300

    Delete Database/DatabaseObjects/StoredProcedures/InsertUser.sql

--- Database/DatabaseObjects/StoredProcedures/InsertUser.sql
+++
@@ -1,8 +0,0 @@
-CREATE PROCEDURE InsertUser
-	@Email VARCHAR(100),
-	@Username VARCHAR(50)
-AS
-BEGIN
-	INSERT INTO [User] (Email, UserName, CreateUser, CreateDate, LastUpdateUser, LastUpdateDate)
-	VALUES (@Email, @UserName, ORIGINAL_LOGIN(), GETDATE(), ORIGINAL_LOGIN(), GETDATE());
- END
