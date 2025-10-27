8e3f94960e2f62769efe91d9c57036952b3e8641
Lucia <106497242+LuciaRepos@users.noreply.github.com>
2025-10-27 18:01:47 -0300

    Delete Database/DatabaseObjects/StoredProcedures/Topic Stored Procedures/GetTopicByID.sql

--- Database/DatabaseObjects/StoredProcedures/Topic Stored Procedures/GetTopicByID.sql
+++
@@ -1,8 +0,0 @@
-CREATE PROCEDURE GetTopicByID
-	@TopicID INT
-AS 
-BEGIN
-	SELECT TopicID, Theme, Content, CreateUser, CreateDate, LastUpdateUser, LastUpdateDate
-	FROM [Topic]
-	WHERE TopicID = @TopicID;
-END
