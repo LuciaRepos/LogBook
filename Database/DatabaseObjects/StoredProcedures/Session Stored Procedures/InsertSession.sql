f2202ac30c958361d23b13e93a74b40b24babcc2
Lucia <106497242+LuciaRepos@users.noreply.github.com>
2025-10-27 21:12:21 -0300

    Update InsertSession.sql

--- Database/DatabaseObjects/StoredProcedures/Session Stored Procedures/InsertSession.sql
+++ Database/DatabaseObjects/StoredProcedures/Session Stored Procedures/InsertSession.sql
@@ -1,1 +1,10 @@
-
+CREATE PROCEDURE InsertSession
+	@TopicID INT,
+	@SessionDate DATETIME,
+	@DurationMinutes INT, 
+	@SessionDescription VARCHAR (500),
+AS
+BEGIN
+	INSERT INTO [Session] (TopicID, SessionDate, DurationMinutes, SessionDescription, CreateUser, CreateDate, LastUpdateUser, LastUpdateDate)
+	VALUES (@TopicID, @SessionDate, @DurationMinutes, @SessionDescription, ORIGINAL_LOGIN(), GETDATE(), ORIGINAL_LOGIN(), GETDATE());
+ END
