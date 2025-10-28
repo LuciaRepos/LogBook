7471de01b55648d3262fd8c5a163b4ca9caa43fa
Lucia <106497242+LuciaRepos@users.noreply.github.com>
2025-10-27 21:07:11 -0300

    Update InsertExercise.sql

--- Database/DatabaseObjects/StoredProcedures/Exercise Stored Procedures/InsertExercise.sql
+++ Database/DatabaseObjects/StoredProcedures/Exercise Stored Procedures/InsertExercise.sql
@@ -4,6 +4,6 @@ CREATE PROCEDURE InsertExercise
 	@Result VARCHAR(500)
 AS
 BEGIN
-	INSERT INTO [Exercise] (TopicID, ExerciseDescription, Result, CreateDate, LastUpdateUser, LastUpdateDate)
+	INSERT INTO [Exercise] (TopicID, ExerciseDescription, Result, CreateUser, CreateDate, LastUpdateUser, LastUpdateDate)
 	VALUES (@TopicID, @ExerciseDescription, @Result, ORIGINAL_LOGIN(), GETDATE(), ORIGINAL_LOGIN(), GETDATE());
  END
