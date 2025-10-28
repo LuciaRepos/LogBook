30936f90a2368071db13d1bbfa75798f1bf77fd3
Lucia <106497242+LuciaRepos@users.noreply.github.com>
2025-10-27 21:45:53 -0300

    Update InsertQuestion.sql

--- Database/DatabaseObjects/StoredProcedures/Question Stored Procedures/InsertQuestion.sql
+++ Database/DatabaseObjects/StoredProcedures/Question Stored Procedures/InsertQuestion.sql
@@ -1,1 +1,11 @@
-
+CREATE PROCEDURE InsertQuestion
+	@TopicID INT,
+	@QuestionStatement VARCHAR(150),
+	@QuestionDate DATETIME,
+	@AnswerDate DATETIME,
+	@Answer VARCHAR (600)
+AS
+BEGIN
+	INSERT INTO [Question] (TopicID, QuestionStatement, QuestionDate, AnswerDate, Answer, CreateUser, CreateDate, LastUpdateUser, LastUpdateDate)
+	VALUES (@TopicID, @QuestionStatement, @QuestionDate, @AnswerDate, @Answer, ORIGINAL_LOGIN(), GETDATE(), ORIGINAL_LOGIN(), GETDATE());
+ END
