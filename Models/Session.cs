42d3a47016386ad3a274d0b4f1518e37fcc2e3ad
Lucia <106497242+LuciaRepos@users.noreply.github.com>
2025-12-17 17:55:19 -0300

    Update Session.cs

--- Models/Session.cs
+++ Models/Session.cs
@@ -3,11 +3,8 @@ namespace LogBookAPI.Models
     public class Session
     {
         public int Id { get; set; }
-
         public int SessionDate { get; set; }
-
         public int DurationMinutes { get; set; }
-
         public string SessionDescription { get; set; }
     }
 }
