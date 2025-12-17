18c6752837d1e39d1bfd957e24801f1b1a99c657
Lucia <106497242+LuciaRepos@users.noreply.github.com>
2025-12-17 16:46:14 -0300

    Create Session.cs

---
+++ Models/Session.cs
@@ -0,0 +1,13 @@
+namespace LogBookAPI.Models
+{
+    public class Session
+    {
+        public int Id { get; set; }
+
+        public int SessionDate { get; set; }
+
+        public int DurationMinutes { get; set; }
+
+        public string SessionDescription { get; set; }
+    }
+}
