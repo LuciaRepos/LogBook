4c9672296e0fc9870735a946b5dbc21a49249d89
Lucia <106497242+LuciaRepos@users.noreply.github.com>
2025-12-17 16:46:54 -0300

    Create Exercise.cs

---
+++ Models/Exercise.cs
@@ -0,0 +1,11 @@
+namespace LogBookAPI.Models
+{
+    public class Exercise
+    {
+        public int Id { get; set; }
+
+        public string ExerciseDescription { get; set; }
+
+        public string Result { get; set; }
+    }
+}
