d0ad398325b380a645c375ed3b586b8a343ccd9c
Lucia <106497242+LuciaRepos@users.noreply.github.com>
2025-12-17 17:56:21 -0300

    Update User.cs

--- Models/User.cs
+++ Models/User.cs
@@ -3,6 +3,7 @@ namespace LogBookAPI.Models
     public class User
     {
         public int Id { get; set; }
+        
         public string Email { get; set; }
 
         public string UserName { get; set; }
