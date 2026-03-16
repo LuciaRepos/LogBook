b3fd994d64dc69b980cc0eed75a00754892b77f6
Lucia <106497242+LuciaRepos@users.noreply.github.com>
2026-03-16 18:53:28 -0300

    Update TopicController.cs

--- Controllers/TopicController.cs
+++ Controllers/TopicController.cs
@@ -1,1 +1,95 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using LogBookAPI.Data;
+using System.Collections.Generic;
+using System.Linq;
+using LogBookAPI.Models;
 
+namespace LogBookAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TopicController : ControllerBase
+    {
+        private readonly LogBookContext _context;
+
+        public TopicController(LogBookContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/topic
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Topic>>> GetTopic()
+        {
+            return await _context.Topics.ToListAsync();
+        }
+
+        // GET: api/topic/1
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Topic>> GetTopic(int id)
+        {
+            var topic = await _context.Topics.FindAsync(id);
+            if (topic == null)
+            {
+                return NotFound();
+            }
+            return topic;
+        }
+
+        // POST: api/topic
+        [HttpPost]
+        public async Task<ActionResult<Topic>> CreateTopic(Topic topic)
+        {
+            _context.Topics.Add(topic);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetTopic), new { id = topic.Id }, topic);
+        }
+
+        // PUT: api/products/1
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateTopic(int id, Topic topic)
+        {
+            if (id != topic.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(topic).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TopicExists(id))
+                {
+                    return NotFound();
+                }
+                throw;
+            }
+            return NoContent();
+        }
+
+        //DELETE: api/products/1
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTopic(int id)
+        {
+            var topic = await _context.Topics.FindAsync(id);
+            if (topic == null)
+            {
+                return NotFound(); 
+            }
+            _context.Topics.Remove(topic);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TopicExists(int id)
+        {
+            return _context.Topics.Any(t => t.Id == id);
+        }
+    }
+}
